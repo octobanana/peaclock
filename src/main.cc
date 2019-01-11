8b24e120dcc41dbf90ce7805856e991758ed3bda
Brett Robinson <octo.banana93@gmail.com>
2019-01-10 22:30:23 -0700

    return exit code on error

--- src/main.cc
+++ src/main.cc
@@ -108,10 +108,12 @@ int main(int argc, char *argv[])
   catch(std::exception const& e)
   {
     std::cerr << "Error: " << e.what() << "\n";
+    return 1;
   }
   catch(...)
   {
     std::cerr << "Error: an unexpected error occurred\n";
+    return 1;
   }
 
   return 0;
