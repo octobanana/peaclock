dae15125f5effa23b893fccf381a1705608285b9
Brett Robinson <octo.banana93@gmail.com>
2019-02-07 00:57:22 -0700

    add option to print license to stdout

--- src/main.cc
+++ src/main.cc
@@ -20,6 +20,8 @@ int program_options(Parg& pg)
   pg.description("A colourful binary clock for the terminal.");
   pg.usage("[-v|--version]");
   pg.usage("[-h|--help]");
+  pg.usage("[-v|--version]");
+  pg.usage("[--license]");
   pg.info("Key Bindings", {
     "q\n    quit the program",
     ":\n    enter the command prompt",
@@ -50,6 +52,7 @@ int program_options(Parg& pg)
     "peaclock",
     "peaclock --help",
     "peaclock --version",
+    "peaclock --license",
   });
   pg.info("Exit Codes", {"0 -> normal", "1 -> error"});
   pg.info("Repository", {
@@ -63,6 +66,7 @@ int program_options(Parg& pg)
   // general flags
   pg.set("help,h", "print the help output");
   pg.set("version,v", "print the program version");
+  pg.set("license", "print the program license");
 
   int status {pg.parse()};
 
@@ -96,6 +100,33 @@ int program_options(Parg& pg)
   if (pg.get<bool>("version"))
   {
     std::cerr << pg.name() << " v" << pg.version() << "\n";
+
+    return 1;
+  }
+
+  if (pg.get<bool>("license"))
+  {
+    std::cerr << R"(MIT License
+
+Copyright (c) 2019 Brett Robinson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.)" << "\n";
 
     return 1;
   }
