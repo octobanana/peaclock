526f09e814368fde2aa9a9c21cd26bbcba06e167
Brett Robinson <octo.banana93@gmail.com>
2020-03-25 18:17:40 -0600

    update parg library

--- src/ob/parg.hh
+++ src/ob/parg.hh
@@ -1,26 +1,47 @@
-//
-// MIT License
-//
-// Copyright (c) 2018-2019 Brett Robinson
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
+/*
+                                    88888888
+                                  888888888888
+                                 88888888888888
+                                8888888888888888
+                               888888888888888888
+                              888888  8888  888888
+                              88888    88    88888
+                              888888  8888  888888
+                              88888888888888888888
+                              88888888888888888888
+                             8888888888888888888888
+                          8888888888888888888888888888
+                        88888888888888888888888888888888
+                              88888888888888888888
+                            888888888888888888888888
+                           888888  8888888888  888888
+                           888     8888  8888     888
+                                   888    888
+
+                                   OCTOBANANA
+
+Licensed under the MIT License
+
+Copyright (c) 2018-2020 Brett Robinson <https://octobanana.com/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #ifndef OB_PARG_HH
 #define OB_PARG_HH
@@ -343,6 +364,10 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
       os.line_wrap(false);
     }
 
+    // uncomment when generating a new help.txt file
+    // os.width(80);
+    // os.line_wrap(true);
+
     os.escape_codes(color_);
     os.indent(2);
     os.first_wrap(false);
@@ -410,7 +435,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         {
           os
           << aec::wrap("=", style.p)
-          << aec::wrap("<" + e.arg_ + ">", style.arg);
+          << aec::wrap("<" + e.arg_ + ">", style.arg)
+          << aec::wrap(" [" + e.value_ + "]", style.val);
         }
 
         os
@@ -686,7 +712,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
   template<typename T,
     std::enable_if_t<
-      std::is_integral_v<T>,
+      // std::is_integral_v<T>,
+      std::is_arithmetic_v<T>,
       int> = 0>
   T get(std::string const& _key)
   {
@@ -846,7 +873,6 @@ private:
   std::string version_;
   std::vector<std::string> usage_;
   std::string description_;
-  int options_indent_ {0};
   std::vector<Output> info_;
   std::string author_;
   std::map<std::string, Option> data_;
